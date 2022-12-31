e2638a26e9c39e85573c56f29edc00c6f3f7684f
looju <omofade2020@gmail.com>
2022-12-31 20:08:37 +0000

    Using tailwind

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useTailwind} from 'tailwind-rn';
 
 export default function App() {
+  const tailwind = useTailwind();
+
   return (
-    <View style={styles.container}>
+    <View style={tailwind('items-center flex-1 justify-center')}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
