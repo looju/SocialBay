5801eff66460e4a9631e9175bc43a0c8e78886f6
looju <omofade2020@gmail.com>
2023-01-23 18:35:09 +0000

    adding navigator

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {useTailwind} from 'tailwind-rn';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ChatScreen } from './Src/Features/Screens/ChatScreen';
+import { HomeScreen } from './Src/Features/Screens/HomeScreen';
+
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const tailwind = useTailwind();
 
   return (
-    <View style={tailwind('items-center flex-1 justify-center')}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
---
+++ Src/Features/Screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Homescreen</Text>
+    </View>
+  )
+}
+
