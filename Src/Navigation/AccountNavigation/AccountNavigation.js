b289d03ba0e5da6d4435b84004e6719cb99639a9
looju <omofade2020@gmail.com>
2023-01-27 10:28:26 +0000

    defininf header icon

--- Src/Navigation/AccountNavigation/AccountNavigation.js
+++ Src/Navigation/AccountNavigation/AccountNavigation.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { LoginScreen } from './../../Features/Screens/LoginScreen';
+import { LoginScreen } from "./../../Features/Screens/LoginScreen";
 
 const Stack = createStackNavigator();
 
 export const AccountNavigation = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen}  options={{}}/>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{
+          headerTintColor: "#fff",
+          headerTitleAlign: "center",
+          headerStyle: { backgroundColor: "#fff" },
+          headerTitle: () => (
+            <Image
+              style={{ height: 50, width: 50 }}
+              source={require("../../../assets/icon.jpg")}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 };
