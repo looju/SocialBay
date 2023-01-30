511c644a2ca9af52cc3e0181c86cf229493ef4a3
looju <omofade2020@gmail.com>
2023-01-30 01:40:05 +0000

    Finsihing home screen

--- Src/Features/Screens/CarouselScreen.js
+++ Src/Features/Screens/CarouselScreen.js
@@ -1,61 +1,34 @@
-import { View, Text, StyleSheet,Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import React, { useRef } from "react";
-import Carousel from "react-native-reanimated-carousel";
 
-export const CarouselScreen = () => {
-  const Data = [
-    {
-      id: "1",
-      title: "After Everything Official Teaser (2023) After 5",
-      thumbnail:
-        "https://unsplash.com/photos/xqC7hdLMpgk",
-    },
-    {
-      id: "2",
-      title: "The Tomorrow Job Official Trailer (2023)",
-      thumbnail:
-        "https://unsplash.com/photos/wK2lihxIwZ0",
-    },
-    {
-      id: "3",
-      title: "Imani Official Trailer (2023)",
-      thumbnail:
-        "https://unsplash.com/photos/g1Kr4Ozfoac",
-    },
-    
-  ];
-
+export const CarouselScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.button}>
-        <Text>CarouselScreen</Text>
+    <ImageBackground
+      style={styles.container}
+      source={require("../../../assets/people2.jpg")}
+      resizeMode="cover"
+    >
+      <View style={styles.iconView}>
+        <Image
+          source={require("../../../assets/icon.jpg")}
+          style={styles.image}
+        />
       </View>
-      <View sttyle={styles.Carousel}>
-      <Carousel
-      data={Data}
-      loop
-      autoPlay={true}
-      width={Dimensions.get("screen").height*0.5}
-      height={Dimensions.get("screen").height*0.5}
-      scrollAnimationDuration={1500}
-      style={styles.Carousel}
-      renderItem={({ index }) => (
-        <View
-            style={{
-                flex: 1,
-                borderWidth: 1,
-                justifyContent: 'center',
-            }}
-        >
-            <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                {index.title}
-            </Text>
-        </View>
-    )}
-      />
-      </View>
-     
-    </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Login")}
+      >
+        <Text style={styles.text}>Sign in and connect</Text>
+      </TouchableOpacity>
+    </ImageBackground>
   );
 };
 
@@ -63,11 +36,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  button:{
-  top:Dimensions.get("screen").height*0.9
+  button: {
+    top: Dimensions.get("screen").height * 0.75,
+    backgroundColor: "#ff5349",
+    height: 60,
+    width: Dimensions.get("screen").width,
+    borderRadius: 10,
+    borderColor: "#fff",
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
   Carousel: {
-    marginTop:Dimensions.get("screen").height*0.15,
-    
+    marginTop: Dimensions.get("screen").height * 0.15,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 15,
+  },
+  iconView: {
+    backgroundColor: "rgba(255,255,255)",
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    height: 60,
+    width: 60,
   },
 });
