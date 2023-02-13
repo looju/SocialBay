abce63e7bb2aab98b42ffa8d833d2c6d03ac1a38
looju <omofade2020@gmail.com>
2023-02-13 02:25:48 +0000

    Creating match screen

--- Src/Features/Screens/App/MatchScreen.js
+++ Src/Features/Screens/App/MatchScreen.js
@@ -1,16 +1,32 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity,Dimensions } from "react-native";
 import React from "react";
 
-export const MatchScreen = ({ route }) => {
+export const MatchScreen = ({ route, navigation }) => {
   const { loggedInProfiles, userSwiped } = route.params;
   return (
     <View style={Styles.container}>
       <View style={Styles.headerTitle}>
-        <Image source={{ uri: "https://links.papareact.com/mg9" }} />
+        <Image
+          source={{ uri: "https://links.papareact.com/mg9" }}
+          style={Styles.headerImageStyle}
+        />
       </View>
       <Text style={Styles.matchText}>
         You and {userSwiped.name} have matched each other!
       </Text>
+      <View style={Styles.imageView}>
+        <Image
+          style={Styles.imageStyle}
+          source={{ uri: loggedInProfiles.photo }}
+        />
+        <Image style={Styles.imageStyle} source={{ uri: userSwiped.photo }} />
+      </View>
+      <TouchableOpacity
+        style={Styles.button}
+        onPress={() => navigation.navigate("Chat")}
+      >
+        <Text style={Styles.buttonText}>Send a message</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -26,9 +42,37 @@ const Styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingTop: 20,
   },
+  headerImageStyle: {
+    width:Dimensions.get("screen").width,
+height:20,
+
+},
   matchText: {
     marginTop: 5,
     color: "#fff",
+    textAlign: "center",
+  },
+  imageView: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 5,
+  },
+  imageStyle: {
+    height: 32,
+    width: 32,
+    borderRadius: 16,
+  },
+  button: {
+    backgroundColor: "#fff",
+    margin: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    marginTop: 20,
+    border: 1,
+    borderColor: "#fff",
+    borderRadius: 5,
+  },
+  buttonText: {
     textAlign: "center",
   },
 });
