302faf6dc2fb1aa22fc11c1e5f228d5d39ea3a3e
looju <omofade2020@gmail.com>
2023-02-20 03:42:17 +0000

    utilizing helper fnction

--- Src/Component/DisplayChat.js
+++ Src/Component/DisplayChat.js
@@ -1,20 +1,51 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Services/Auth/Auth";
 import { GetMatchedUserInfo } from "../Lib/GetMatchedUserInfo";
 
 export const DisplayChat = ({ matchedUser, navigation }) => {
   const { user } = useContext(AuthContext);
-  const [matchedUserInfo, setMatchedUserInfo] = useState([]);
+  const [matchedUserInfo, setMatchedUserInfo] = useState(null);
 
   useEffect(() => {
-    setMatchedUserInfo(GetMatchedUserInfo(matchedUser.user, user.user.uid));
+    setMatchedUserInfo(GetMatchedUserInfo(matchedUser.users, user.user.uid));
   }, [matchedUser, user]);
 
+  console.log(matchedUserInfo);
+
   return (
-    <TouchableOpacity>
-      {/* <Image source={{ uri: `${matchedUser.users.photo}` }} /> */}
-      <Text>hiiiiiiiii</Text>
+    <TouchableOpacity style={Styles.userOverview}>
+      <Image
+        style={{
+          height: 60,
+          width: 60,
+          borderRadius: 30,
+          marginRight: 4,
+        }}
+        source={{ uri: matchedUserInfo?.photo }}
+      />
     </TouchableOpacity>
   );
 };
+
+const Styles = StyleSheet.create({
+  userOverview: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 1.5,
+    marginTop: 10,
+    marginBottom: 20,
+    height: 60,
+    width: "100%",
+    shadowOffset: {
+      width: 0,
+      height: 20,
+    },
+    elevation: 2,
+  },
+});
