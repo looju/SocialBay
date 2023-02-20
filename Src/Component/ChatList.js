f3b6c795009ecfa72ad7e453fc32439f2afa9913
looju <omofade2020@gmail.com>
2023-02-20 02:32:37 +0000

    building  matched users chat

--- Src/Component/ChatList.js
+++ Src/Component/ChatList.js
@@ -1,30 +1,48 @@
-import { View, Text, StyleSheet } from "react-native";
-import React, { useState, useEffect } from "react";
-import { collection, doc, onSnapshot, where } from "firebase/firestore";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../Services/Auth/Auth";
+import { collection, doc, onSnapshot, where, query } from "firebase/firestore";
+import { db } from "../Services/Config/Config";
 
 export const ChatList = () => {
   const [matches, setMatches] = useState([]);
+  const { user } = useContext(AuthContext);
 
-  useEffect(() => {
-    onSnapshot(
-      query(
-        collection(db, "matches"),
-        where("userMatched", "array-contains", user.user.uid)
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "Matches"),
+          where("userMatched", "array-contains", user.user.uid)
+        ),
+        (snapshot) =>
+          setMatches(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          )
       ),
-      (snapshot) =>
-        setMatches(
-          snapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }))
-        )
-    );
-  }, []);
+
+    [user]
+  );
+
+
+
 
   return (
-    <View style={Styles.container}>
-      <Text>ChatList</Text>
-    </View>
+    matches.length>0?(
+      <FlatList
+      data={matches}
+      keyExtractor={item=>item.id}
+      renderItem={(item) => <DisplayChat matchedUser={item}/>}
+    />
+    ):(
+      <View>
+
+      </View>
+    )
+   
   );
 };
 
---
+++ Src/Component/DisplayChat.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DisplayChat = () => {
+  return (
+    <View>
+      <Text>DisplayChat</Text>
+    </View>
+  )
+}
+
+export default DisplayChat
