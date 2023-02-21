9fa3ca77d97a2baa85f4d6c660fa92e083d25c83
looju <omofade2020@gmail.com>
2023-02-21 18:04:01 +0000

    creating states

--- Src/Features/Screens/App/VideoCallScreen.js
+++ Src/Features/Screens/App/VideoCallScreen.js
@@ -1,11 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, Alert, Modal, Text } from "react-native";
+import React, { useState, useEffect, useCallback } from "react";
+import { Button, TextInput } from "react-native-paper";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
+import InCallManager from "react-native-incall-manager";
+import {
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  MediaStream,
+  MediaStreamTrack,
+  mediaDevices,
+  registerGlobals,
+} from "react-native-webrtc";
 
-export const VideoCallScreen = () => {
+export const VideoCallScreen = ({ navigation, ...props }) => {
+  let name;
+  let connectedUser;
+
+  const [userId, setUserId] = useState("");
+  const [socketActive, setSocketActive] = useState(false);
+  const [calling, setCalling] = useState(false);
+
+  //video srcs
+  const [localStream, setLocalStream] = useState({toURL: () => null});
+  const [remoteStream, setRemoteStream] = useState({toURL: () => null});
+  const [conn, setConn] = useState(new WebSocket('ws://3.20.188.26:8080'));
+
   return (
     <View>
       <Text>VideoCallScreen</Text>
     </View>
-  )
-}
-
+  );
+};
