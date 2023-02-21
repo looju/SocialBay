0221a821c81dbbafa6db39c4222d1ba391e49e6b
looju <omofade2020@gmail.com>
2023-02-21 16:01:30 +0000

    storing room id locally

--- Src/Features/Screens/App/IdScreen.js
+++ Src/Features/Screens/App/IdScreen.js
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const IdScreen = () => {
+  const [userId, setUserId] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const login = async () => {
+    setLoading(true);
+    try {
+      await AsyncStorage.setItem("userId", userId);
+      setLoading(false);
+      navigation.navigate("VideoCallScreen");
+    } catch (error) {
+      console.log("Error saving userId" + error);
+    }
+  };
+
   return (
     <View>
       <Text>IdScreen</Text>
     </View>
-  )
-}
-
+  );
+};
